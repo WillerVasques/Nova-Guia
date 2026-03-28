aae7bcd07561a6dcac89d2f528045d77a4e78062
Willer Vasques <53635341+WillerVasques@users.noreply.github.com>
2026-03-28 00:46:38 -0300

    Update image URLs for progressive blur effect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- LÓGICA DE FUNDO PROGRESSIVO (BLUR-UP) ---
 // Substitua estas URLs pelas suas imagens
-const lowResUrl = "https://github.com/WillerVasques/Nova-Guia/blob/main/Imagens/Esquadra-compactado.jpg"; 
-const highResUrl = "https://github.com/WillerVasques/Nova-Guia/blob/main/Imagens/Esquadra-compactado.jpg";
+const lowResUrl = "https://raw.githubusercontent.com/WillerVasques/Nova-Guia/refs/heads/main/Imagens/Esquadra-compactado.jpg"; 
+const highResUrl = "https://raw.githubusercontent.com/WillerVasques/Nova-Guia/refs/heads/main/Imagens/Esquadra.jpg";
 
 const bgLayer = document.getElementById('bg-layer');
 
